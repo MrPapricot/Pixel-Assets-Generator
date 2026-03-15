ebec304545ad1c001aacf6a09b4f2ad9f926bdd7
MrPapricot <118524942+MrPapricot@users.noreply.github.com>
2026-03-16 00:46:40 +0700

    Изменил таблицы в базе данных

--- Database/init_database.d/InitTables.sql
+++ Database/init_database.d/InitTables.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE users (
     uuid uuid PRIMARY KEY DEFAULT uuidv7(),
-    username text NOT NULL,
+    email text NOT NULL UNIQUE,
     password_hash text NOT NULL,
     created_at timestamptz NOT NULL DEFAULT now()
 );
