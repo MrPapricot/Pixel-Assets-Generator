52c9380e9dbba5c94eafaf246dc5246e66b39671
MrPapricot <118524942+MrPapricot@users.noreply.github.com>
2026-05-20 19:17:50 +0700

    Добавил поддержку OpenAPI

--- CommandProjectBackend/src/handlers/mod.rs
+++ CommandProjectBackend/src/handlers/mod.rs
@@ -12,7 +12,7 @@ use serde_json::json;
 
 use utoipa;
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, utoipa::ToSchema)]
 pub(crate) struct UserBody {
     email: String,
     password: String,
@@ -20,12 +20,10 @@ pub(crate) struct UserBody {
 
 #[derive(utoipa::OpenApi)]
 #[openapi(
-    paths(healthcheck),
-    components(
-        schemas(HealthBody)
-    ),
+    paths(healthcheck, create_user_handler, auth_user_handler),
     tags(
-        (name="TESTING", description="Для тестирования. Не использовать")
+        (name="TESTING", description="Для тестирования. Не использовать"),
+        (name="API", description="Для обычного использования"),
     )
 )]
 pub(crate) struct ApiDoc;
@@ -37,13 +35,14 @@ pub(crate) struct HealthBody {
 
     #[serde(rename = "Total services available")]
     total_available: u8,
-    timestamp: String
+    timestamp: String,
 }
 
 #[utoipa::path(
     get,
     path = "/health",
     tags = ["TESTING"],
+    description = "Проверяет работоспособность всех сервисов, и возвращает отчет",
     responses(
         (status = 200, description="Возвращает работоспособность всех сервисов", body=HealthBody)
     )
@@ -64,16 +63,32 @@ pub(crate) async fn healthcheck(State(state): State<AppState>) -> (StatusCode, J
     state.log("Healthcheck requested", LogLevel::Info);
     (
         StatusCode::OK,
-        Json(
-            HealthBody {
-                timestamp,
-                total_available,
-                services: statuses
-            }
-        ),
+        Json(HealthBody {
+            timestamp,
+            total_available,
+            services: statuses,
+        }),
     )
 }
 
+#[derive(utoipa::ToSchema, serde::Serialize)]
+struct UserSuccess {
+    token: String,
+}
+
+#[utoipa::path(
+    post,
+    path = "/create_user",
+    tags = ["API"],
+    request_body = UserBody,
+    description = "Создает пользователя с переданной почтой и паролем",
+    responses(
+        (status = 200, description = "Возвращает токен", body = UserSuccess),
+        (status = 409, description = "Email уже используется", body = String),
+        (status = 500, description = "Внутренняя ошибка сервиса", body = String),
+        (status = 502, description = "Внешнии сервисы не работают", body = String),
+    )
+)]
 #[axum::debug_handler]
 pub(crate) async fn create_user_handler(
     State(state): State<AppState>,
@@ -81,23 +96,38 @@ pub(crate) async fn create_user_handler(
 ) -> impl IntoResponse {
     use CreateUserResult as CUR;
     match state.create_new_user(email, password).await {
-        CUR::EmailUsed => (
-            StatusCode::CONFLICT,
-            Json(json!({"Error": "Email is already user"})),
-        ),
+        CUR::EmailUsed => (StatusCode::CONFLICT, Json(json!("Email is already used"))),
         CUR::BaseError(error) => match error {
             Errors::AuthServiceInternalError
             | Errors::NotFound
             | Errors::AuthServiceUnaccessible => (
                 StatusCode::BAD_GATEWAY,
-                Json(json!({"Error": "Auth service is not working properly"})),
+                Json(json!(
+                    "Some services are not working properly. Try again later"
+                )),
             ),
-            Errors::SelfInternalError(json_info) => (StatusCode::INTERNAL_SERVER_ERROR, json_info),
+            Errors::SelfInternalError(_) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!("Something went wrong. Try again later")),
+            ),
         },
         CUR::UserCreated { token } => (StatusCode::OK, Json(json!({"Token": token}))),
     }
 }
 
+#[utoipa::path(
+    post,
+    path = "/auth_user",
+    tags = ["API"],
+    request_body = UserBody,
+    description = "Ищет пользователя по переданной почте и паролю",
+    responses(
+        (status = 200, description = "Возвращает токен", body = UserSuccess),
+        (status = 401, description = "Пользователя с таким email и паролем нет", body = String),
+        (status = 500, description = "Внутренняя ошибка сервиса", body = String),
+        (status = 502, description = "Внешнии сервисы не работают", body = String),
+    )
+)]
 pub(crate) async fn auth_user_handler(
     State(state): State<AppState>,
     JsonExtractor(UserBody { email, password }): JsonExtractor<UserBody>,
@@ -106,16 +136,19 @@ pub(crate) async fn auth_user_handler(
     match state.get_user_token(email, password).await {
         AUR::NoUserFound => (
             StatusCode::UNAUTHORIZED,
-            Json(json!({"Error": "No user with such email or password"})),
+            Json(json!("No user with such email or password")),
         ),
         AUR::BaseError(error) => match error {
             Errors::AuthServiceInternalError
             | Errors::NotFound
             | Errors::AuthServiceUnaccessible => (
                 StatusCode::BAD_GATEWAY,
-                Json(json!({"Error": "Auth service is not working properly"})),
+                Json(json!("Auth service is not working properly")),
             ),
-            Errors::SelfInternalError(json_info) => (StatusCode::INTERNAL_SERVER_ERROR, json_info),
+            Errors::SelfInternalError(_) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!("Something went wrong. Try again later")),
+            ),
         },
         AUR::UserAuthenticated { token } => (StatusCode::OK, Json(json!({"Token": token}))),
     }
